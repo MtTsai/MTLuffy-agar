a6174fe04a8d14e8090a0653447bcdac9f80a422
MT Tsai <a17653aw96801@gmail.com>
2016-01-06 12:16:28 -0500

    Add score & Method of queryDir change to boradcast

--- server.js
+++ server.js
@@ -37,41 +37,40 @@ function calc_dist(pos1, pos2) {
     return Math.sqrt(Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2));
 }
 
-function cal_radius(rad1, rad2 ){
-    return Math.sqrt( (rad1 * rad1) + (rad2 * rad2) );
-}
-
 function eat_balls(id) {
     // TODO: other player (can be eaten or eat others)
 
     // foods
     for (var i in food_list) {
         if (calc_dist(food_list[i].pos, client_list[id].pos) < client_list[id].radius) {
-            client_list[id].radius = cal_radius( client_list[id].radius, food_list[i].radius);
+            client_list[id].score = client_list[id].score + food_list[i].score;
+            client_list[id].radius = Math.sqrt(client_list[id].score);
             food_list.splice(i, 1);
         }
     }
 }
 
 
-function gen_foods() {
+function food_create() {
     food_list.push({
         pos: [random(0, map.width), random(0, map.height)],
-        radius: 5
+        radius: 5,
+        score: 25
     });
+}
+
+function gen_foods() {
+    food_create();
 
     // generate the food per 10 secs
     setTimeout(gen_foods, 10000);
 }
+gen_foods();
 
 /* initialize */
 for (var i = 0; i < 100; i++) {
-    food_list.push({
-        pos: [random(0, map.width), random(0, map.height)],
-        radius: 5
-    });
+    food_create();
 }
-gen_foods();
 
 /* Handling webSocket */
 io.on('connection', function(socket) {
@@ -81,7 +80,8 @@ io.on('connection', function(socket) {
         socket: socket,
         pos: [random(0, map.width), random(0, map.height)],
         radius: 10,
-        speed: 10
+        speed: 10,
+        score: 100
     };
 
     socket.on('updatePos', function(dir) {
@@ -146,11 +146,12 @@ io.on('connection', function(socket) {
         console.log(pos[0] + ' ' + pos[1]);
     });
 
-    // setting the query interval 25ms
-    function queryDir() {
-        socket.emit('queryDir');
-        setTimeout(queryDir, 25);
-    }
-    queryDir();
 });
+
+// setting the query interval 25ms
+function queryDir() {
+    io.emit('queryDir');
+    setTimeout(queryDir, 25);
+}
+queryDir();
 
